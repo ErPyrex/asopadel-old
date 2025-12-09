355cbde9c023198deccd0f8b795c536c8b111789
Pyrex64 <sergioalarcon22986@gmail.com>
2025-12-09 18:19:16 -0400

    refactor(architecture): decouple frontend and backend
    Moves django app to backend/ and initializes react app in frontend/

--- core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Models have been moved to:
-# - competitions (Torneo, Partido, EstadisticaJugador)
-# - facilities (Cancha, ReservaCancha)
-# - store (Producto)
-# - blog (Hero, Noticia)
